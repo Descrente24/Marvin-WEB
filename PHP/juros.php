add62da7538ccac96381ad880f80cbfbe694c789
Marvin <escorpiano18@bol.com.br>
2024-10-21 09:28:54 -0300

    mudanças

---
+++ PHP/juros.php
@@ -0,0 +1,9 @@
+<?php
+    $capital = $_POST["capital"];
+    $taxa    = $_POST["taxa"];
+    $tempo   = $_POST["tempo"];
+
+    echo "Capital: $capital <br>";
+    echo "Taxa: $taxa <br>";
+    echo "Tempo: $tempo <br>";
+?>
